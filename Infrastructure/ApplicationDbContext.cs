6bca8190822769be118c01b3c91994eb3f52e2fe
黎又铭 <2939828886@qq.com>
2018-08-08 15:19:18 +0800

    1

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -14,21 +14,10 @@ namespace Infrastructure
             : base(options)
         {
         }
-        public DbSet<ClassModel> Classs { get; set; }
-        public DbSet<CourseModel> Courses { get; set; }
-        public DbSet<ExamInfoModel> ExamInfos { get; set; }
-        public DbSet<RoomModel> Rooms { get; set; }
-        public DbSet<TeacherModel> Teachers { get; set; }
-
-        public DbSet<BoxModel> Boxs { get; set; }
+      
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.ApplyConfiguration(new ClassMap());
-            builder.ApplyConfiguration(new CourseMap());
-            builder.ApplyConfiguration(new ExamInfoMap());
-            builder.ApplyConfiguration(new RoomMap());
-            builder.ApplyConfiguration(new TeacherMap());
-            builder.ApplyConfiguration(new BoxMap());
+           
             base.OnModelCreating(builder);
         }
     }
